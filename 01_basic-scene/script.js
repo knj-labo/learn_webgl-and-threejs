2342a382cfef7979d40e2915f288235931370e5f
jp-knj <kenji.tomita1996@gmail.com>
2022-12-27 23:50:03 +0900

    :sparkles: render black dimention why: confirm to view black canvas"

--- 01_basic-scene/script.js
+++ 01_basic-scene/script.js
@@ -1,1 +1,36 @@
-console.log(THREE);
+// like a container
+// put objects, models, light, etc. in it
+// At some point we ask Three.js to render that scene
+
+// Scene
+const scene = new THREE.Scene();
+
+// Red cube and Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+
+scene.add(mesh)
+
+// not visible
+// serve as point of view when doing a render
+// different types
+// going t use PerspectiveCamera
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+scene.add(camera)
+
+// Renderer
+const canvas = document.querySelector(".webgl")
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
