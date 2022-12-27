d62c0bdb04b4ebded68915718865948863a957dd
jp-knj <kenji.tomita1996@gmail.com>
2022-12-28 00:21:18 +0900

    :sparkles: setup webpack why: improve development experimence

--- 01_basic-scene/src/script.js
+++ 01_basic-scene/src/script.js
@@ -1,34 +1,38 @@
-// like a container
-// put objects, models, light, etc. in it
-// At some point we ask Three.js to render that scene
+import './style.css'
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
-// Red cube and Object
+/**
+ * Object
+ */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
-
 scene.add(mesh)
 
-// not visible
-// serve as point of view when doing a render
-// different types
-// going t use PerspectiveCamera
-
-// Sizes
+/**
+ * Sizes
+ */
 const sizes = {
     width: 800,
     height: 600
 }
 
-// Camera
+/**
+ * Camera
+ */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
 scene.add(camera)
 
-// Renderer
-const canvas = document.querySelector(".webgl")
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
