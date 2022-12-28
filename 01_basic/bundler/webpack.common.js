cb9c2f4db761b6d2d75713796c09d42f76032343
jp-knj <kenji.tomita1996@gmail.com>
2022-12-28 23:27:16 +0900

    :tada: initial project2

--- 01_basic/bundler/webpack.common.js
+++ 01_basic/bundler/webpack.common.js
@@ -4,7 +4,7 @@ const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
 module.exports = {
-    entry: path.resolve(__dirname, '../src/script01.js'),
+    entry: path.resolve(__dirname, '../src/script02.js'),
     output:
     {
         hashFunction: 'xxhash64',
---
+++ 01_basic/src/script02.js
@@ -0,0 +1,39 @@
+import './style.css'
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+
+/**
+ * Axes Helper
+ */
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
