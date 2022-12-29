0276dafc6943df7ccbd8adae3e9bf195971057ea
jp-knj <kenji.tomita1996@gmail.com>
2022-12-29 22:37:46 +0900

    :tada: initialize project

--- 01_basic/bundler/webpack.common.js
+++ 01_basic/bundler/webpack.common.js
@@ -4,7 +4,7 @@ const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
 module.exports = {
-    entry: path.resolve(__dirname, '../src/script07.js'),
+    entry: path.resolve(__dirname, '../src/script08.js'),
     output:
     {
         hashFunction: 'xxhash64',
