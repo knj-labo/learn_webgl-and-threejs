c2c1654987614f281edda85f1fd60a5f37eeddcf
jp-knj <kenji.tomita1996@gmail.com>
2022-12-30 19:14:28 +0900

    :art: refactoring to read easily

--- 01_basic/src/experience/experience.js
+++ 01_basic/src/experience/experience.js
@@ -1,15 +1,24 @@
 import Sizes from './utils/sizes.js'
+import EventEmitter from "./utils/event-emitter";
 
 export default class Experience {
     constructor(canvas) {
-      // global access
-      window.experience = this
-      // option
-      this.canvas = canvas
-      // setup
-      this.sizes = new Sizes()
-      console.log(this.sizes.width)
-      console.log(this.sizes.height)
-      console.log(this.sizes.pixelRatio)
+        // global access
+        window.experience = this
+        // option
+        this.canvas = canvas
+
+        // setup
+        this.sizes = new Sizes()
+        console.log(this.sizes.width)
+        console.log(this.sizes.height)
+        console.log(this.sizes.pixelRatio)
+
+        // setup
+        this.sizes.on('resize', this.resize)
+    }
+
+    resize() {
+        console.log('A resize occurred')
     }
 }
