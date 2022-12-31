7886252fd195d3376fd23d591c6de36d7d56b5e6
jp-knj <kenji.tomita1996@gmail.com>
2022-12-31 15:11:52 +0900

    :sparkles: done

---
+++ 01_basic/src/experience/world/environment.js
@@ -0,0 +1,20 @@
+import { DirectionalLight } from 'three'
+import Experience from '../experience.js'
+
+export default class Environment {
+    constructor() {
+        this.experience = new Experience()
+        this.scene = this.experience.scene
+        this.setSunLight()
+    }
+
+    setSunLight() {
+        this.sunLight = new DirectionalLight('#ffffff', 4)
+        this.sunLight.castShadow = true
+        this.sunLight.shadow.camera.far = 15
+        this.sunLight.shadow.mapSize.set(1024, 1024)
+        this.sunLight.shadow.normalBias = 0.05
+        this.sunLight.position.set(3, 3, - 2.25)
+        this.scene.add(this.sunLight)
+    }
+}
--- 01_basic/src/experience/world/world.js
+++ 01_basic/src/experience/world/world.js
@@ -1,17 +1,20 @@
 import { Mesh, BoxGeometry, MeshBasicMaterial } from 'three'
 import Experience from '../experience.js'
+import Environment from './environment.js';
 
 export default class World {
     constructor() {
         this.experience = new Experience()
         this.scene = this.experience.scene
 
-
         // Test mesh
         const testMesh = new Mesh(
             new BoxGeometry(1, 1, 1),
-            new MeshBasicMaterial({ wireframe: true })
+            new MeshBasicMaterial()
         )
         this.scene.add(testMesh)
+
+        // Setup
+        this.environment = new Environment()
     }
 }
